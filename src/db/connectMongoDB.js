61edbb2a2ffbebf13c24638c7366c036a9bbf270
Viktor Liulchyk <viktor.lyulchik@gmail.com>
2025-09-18 00:06:17 +0300

    add task solution

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   try {
-    const mongoUrl = process.env.MONGODB_URL;
+    const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
-    process.exit(1); // аварійне завершення програми
+    process.exit(1);
   }
 };
--- src/server.js
+++ src/server.js
@@ -5,23 +5,19 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import studentsRoutes from './routes/studentsRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Глобальні middleware
-app.use(logger); // 1. Логер першим — бачить усі запити
-app.use(express.json()); // 2. Парсинг JSON-тіла
-app.use(cors()); // 3. Дозвіл для запитів з інших доменів
+app.use(logger);
+app.use(express.json());
+app.use(cors());
 
-// підключаємо групу маршрутів студента
-app.use(studentsRoutes);
+app.use(notesRoutes);
 
-// 404 — якщо маршрут не знайдено
 app.use(notFoundHandler);
 
-// Error — якщо під час запиту виникла помилка
 app.use(errorHandler);
 
 await connectMongoDB();
